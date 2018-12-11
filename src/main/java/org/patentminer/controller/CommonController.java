df4ee64f34752407403027f9cbe86d47dc72a219
robin <robinchow8991@gmail.com>
2018-12-11 23:15:04 +0800

    update swagger

--- src/main/java/org/patentminer/controller/CommonController.java
+++ src/main/java/org/patentminer/controller/CommonController.java
@@ -1,9 +1,11 @@
 package org.patentminer.controller;
 
+import io.swagger.annotations.Api;
 import org.patentminer.bean.ResultBean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "通用")
 @RestController
 public class CommonController {
 
